3d467949162b5d041d5235ff8ca4e8c60c028384
Sam Lempp <slempp@live.unc.edu>
2020-10-07 17:13:11 -0400

    GHPages and React fix 3

---
+++ build/precache-manifest.ead333d47b04bb719b9f77a518a3d2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f5bf66fa6a87daacaeaa948a4b91b4",
+    "url": "/piccup/index.html"
+  },
+  {
+    "revision": "afcc7d3e52b2d911594f",
+    "url": "/piccup/static/css/main.ef8c080a.chunk.css"
+  },
+  {
+    "revision": "ef8862a6e50c4fcc40e2",
+    "url": "/piccup/static/js/2.12253d35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piccup/static/js/2.12253d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcc7d3e52b2d911594f",
+    "url": "/piccup/static/js/main.2611ee16.chunk.js"
+  },
+  {
+    "revision": "65dd18c193d9384bb5a8",
+    "url": "/piccup/static/js/runtime-main.2bce9208.js"
+  },
+  {
+    "revision": "4adaf684d9f8ce9ebc3e24cd9227c310",
+    "url": "/piccup/static/media/logo.4adaf684.svg"
+  }
+]);
