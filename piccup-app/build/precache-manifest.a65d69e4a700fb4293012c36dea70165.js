29a43eb9b9d22e37e081b9bd6affaae8a214464d
Sam Lempp <slempp@live.unc.edu>
2020-10-07 16:24:58 -0400

    pages test 2

---
+++ piccup-app/build/precache-manifest.a65d69e4a700fb4293012c36dea70165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5068864801d53bd0ab404b0feed3b9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e425a32f0653dcaf8825",
+    "url": "/my-app/static/css/main.ef8c080a.chunk.css"
+  },
+  {
+    "revision": "1340928f6db5269abd87",
+    "url": "/my-app/static/js/2.719c570c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.719c570c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e425a32f0653dcaf8825",
+    "url": "/my-app/static/js/main.4dd66629.chunk.js"
+  },
+  {
+    "revision": "d782912293f1a5e078aa",
+    "url": "/my-app/static/js/runtime-main.156f03ed.js"
+  },
+  {
+    "revision": "4adaf684d9f8ce9ebc3e24cd9227c310",
+    "url": "/my-app/static/media/logo.4adaf684.svg"
+  }
+]);
